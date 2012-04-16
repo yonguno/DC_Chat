124caad49b5f07a59a5612f44214e298b09545b6
Ji-Hun Cho <jihun268@gmail.com>
2012-04-16 11:57:16 -0700

    changed style, name cannot start with '>' <- those for notices

--- App3.js
+++ App3.js
@@ -18,18 +18,22 @@ app.configure(function(){
 		});
 	});
 });
+
 var text = new Array(10);
+
 function log(str){
 	for(var i=0;i<9;i++){
 		text[i] = text[i+1];
 	}
 	text[9] = str;
 }
+
 var users = {};
 var sessions = {};
 var connections = {};
 app.listen(2020);
 var sio=io.listen(app);
+
 sio.set('authorization',function(data,accept){
 	if(data.headers.cookie){
 		data.cookie = parseCookie(data.headers.cookie);
@@ -39,17 +43,21 @@ sio.set('authorization',function(data,accept){
 	}
 	accept(null,true);
 });
+
 sio.sockets.on('connection',function(socket){
-	sessionID = socket.handshake.sessionID;
+	var sessionID = socket.handshake.sessionID;
+	
 	if(connections[sessionID]){
-	  connections[sessionID].emit('news',{msg:'You opened new session. You are no longer use this session.'});
-		socket.emit('news',{msg:'You have duplicated connections. Your old session will be disconnected.'});
+	  connections[sessionID].emit('news',{msg:'>ERROR: You opened new session. You are no longer use this session.'});
+		socket.emit('news',{msg:'>NOTICE: You have duplicated connections. Your old session will be disconnected.'});
 		connections[sessionID].disconnect();
 	}
-	connections[sessionID] = socket;
+
+  connections[sessionID] = socket;
 	console.log(""+sessionID+" connected!");
+
   if(sessions[sessionID]){
-		username = sessions[sessionID];
+		var username = sessions[sessionID];
 		while(users[username]!=undefined){
 			username = "중복_"+username;
 		}
@@ -61,58 +69,75 @@ sio.sockets.on('connection',function(socket){
 		}while(users[username]!=undefined);
 		sessions[sessionID] = username;
 	}
-	sio.sockets.emit('news',{msg:'Notice: '+username+' has joined the chat.'});
+	sio.sockets.emit('news',{msg:'>NOTICE: '+username+' has joined the chat.'});
 
   socket.emit('name',{name:username});
 	users[username] = socket; 
-	if(text[9]!=undefined){
+
+  if(text[9]!=undefined){
 		var logs = "";
-		for(var i=0;i<10;i++){
+
+    for(var i=0;i<10;i++){
 			if(text[i]!=undefined)
 				socket.emit('news',{msg:text[i].replace(/</g,'&lt;').replace(/>/g,'&gt;')});
 		}
 	}
-	userlist = "";
-	for(user in users){userlist +='<div>'+user+'</div>';};
+
+  var userlist = "";
+
+	for(user in users){
+	  userlist +='<div>'+user+'</div>';
+	};
+
 	sio.sockets.emit('users',{users:userlist});
+	
 	socket.on('msg',function(data){
 		log(username+":"+data.msg);
 	  var re = new RegExp(/(\/t)\s([^\s]+)\s(.*)$/);
 		if(re.test(data.msg.replace(/\s+$/,''))) {
 		  tell = re.exec(data.msg.replace(/</g,'&lt;').replace(/>/g,'&gt;'));
+			
 			if(tell[2] == username) {
-			  socket.emit('news',{msg:" Talking to ourselves, are we?"});
+			  socket.emit('news',{msg:">Talking to ourselves, are we?"});
 			}else{
 			  try {
           users[tell[2]].emit('news',{msg:" tell from "+username+": "+tell[3]});
-			    socket.emit('news',{msg:" tell sent to "+tell[2]+": "+tell[3]});
+			    socket.emit('news',{msg:">Tell sent to "+tell[2]+": "+tell[3]});
 			  } catch (err) {
-				  socket.emit('news',{msg:"Invaild username."});
+				  socket.emit('news',{msg:">ERROR: Invaild username."});
 				}
 			}
 		}else{
 		  var re = new RegExp(/(\/t)\s([^\s]+)/);
-		  if(!re.test(data.msg.replace(/\s+$/,''))) {
+		  
+			if(!re.test(data.msg.replace(/\s+$/,''))) {
 			  sio.sockets.emit('news',{msg:username+":"+data.msg});
 	    }
 		}
 	});
+	
 	socket.on('name',function(data){
 		prevName=username;
 		delete users[username];
-		username = data.name.replace(/</g,'&lt;').replace(/>/g,'&gt;');
+		username = data.name.replace(/^(&lt;|>)+/g,'');
+		
 		while(users[username]!=undefined){
 			username = "중복_"+username;
 		}
+		
 		users[username] = socket;
 		sessions[sessionID] = username;
 		socket.emit('name',{name:username});
 		log(prevName+"->"+username);
 		sio.sockets.emit('news',{msg:prevName+"->"+username});
 		userlist = "";
-		for(user in users){userlist +='<div>'+user+'</div>';}
+		
+		for(user in users){
+		  userlist +='<div>'+user+'</div>';
+		}
 		sio.sockets.emit('users',{users:userlist});
 	});
+
 	socket.on('disconnect',function(){
 		delete users[username];
 		connections[sessionID] = false;
